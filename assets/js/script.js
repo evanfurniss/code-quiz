0a671e73123b42a4f3a43bcb282e97eb202b734b
Evan Furniss <evanfurniss@gmail.com>
2021-01-08 08:58:40 -0600

    Created a timer that displays on the page and auto updates the seconds remaining. Stopping at zero and displaying a game over message

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,3 +39,33 @@
     //score.html
       //capture the score form the local storage and display it to user
       //reset the local storage with 0.
+var textBox = document.querySelector("#question");
+var timer = document.querySelector("#timer");
+var btnClick = document.querySelector(".btn");
+var secRemaining = 90;
+
+document.querySelector("#btnStart").addEventListener("click", function(e){
+    e.preventDefault();
+
+    timerFunc;
+});
+
+var timerFunc = setInterval(function(){
+    secRemaining--;
+    timer.textContent = ("Time: " + secRemaining);
+
+    if (secRemaining === 0) {
+        clearInterval(timerFunc);
+        textBox.textContent = "Game Over :(";
+    }
+}, 1000);
+
+// function writeQuestions(e) {
+//     e.preventDefault();
+
+//     for (let i = 1; i < qList.length; i++) {
+//         textBox.textContent = qList.question1;
+//     }
+// }
+
+// btnClick.addEventListener("click", writeQuestions);
