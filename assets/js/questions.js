be2873c4aff9ed1cc39148231ab22c4ff61b1576
Evan Furniss <evanfurniss@gmail.com>
2021-01-08 17:44:16 -0600

    Created working functions to add questions, buttons, and check answers of questions.

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,25 +1,25 @@
 var qList = [{
-    "question1": "What is the name of the playable character in Halo: Combat Evolved?",
-    "answers":["John Adams", "Marcus Phoenix", "Master Chief", "Crash Bandicoot"],
-    "correctAnswer":"Master Chief"
+    question: "What is the name of the playable character in Halo: Combat Evolved?",
+    answers: ["John Adams", "Marcus Phoenix", "Master Chief", "Crash Bandicoot"],
+    correctAnswer: "Master Chief"
 }, 
 {
-    "question2":"what is the name of the Halo multiplayer map?",
-    "answers":["Blood Gulch", "Battle Creek", "Sidewinder", "Wizard"],
-    "correctAnswer": "Blood Gulch"
+    question: "what is the name of the Halo multiplayer map?",
+    answers: ["Blood Gulch", "Battle Creek", "Sidewinder", "Wizard"],
+    correctAnswer:  "Blood Gulch"
 },
 {
-    "question3":"What is the name of the antagonist group",
-    "answers":["Flood", "Covenant", "Swarm", "Both a & b"],
-    "correctAnswer": "Both a & b"
+    question: "What is the name of the antagonist group",
+    answers: ["Flood", "Covenant", "Swarm", "Both a & b"],
+    correctAnswer: "Both a & b"
 },
 {
-    "question4":"What is the name of the Captain you save?",
-    "answers":["Baird","Prescot","Wallace","Keyes"],
-    "correctAnswer":"Keyes"
+    question: "What is the name of the Captain you save?",
+    answers: ["Baird","Prescot","Wallace","Keyes"],
+    correctAnswer: "Keyes"
 },
 {
-    "question5":"What is the name of floating orb?",
-    "asnwers":["Chaos Emerald", "Naughty Splash", "Guilty Spark", "Planet Core"],
-    "correctAnswer":"Guilty Spark"
+    question: "What is the name of floating orb?",
+    asnwers: ["Chaos Emerald", "Naughty Splash", "Guilty Spark", "Planet Core"],
+    correctAnswer: "Guilty Spark"
 }];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,48 @@
-// create 2 html files
-    // game.html
-         //qList = [{
-                                //        question:"",
-                                //        options:[op1,op2,op3,op4],
-                                //        correctAns: op1
-                                //    },
-                                // {
-                                    //        question:"",
-                                    //        options:[op1,op2,op3,op4],
-                                    //        correctAns: op1
-                                    //    },
-                            // ]
-        //timer = 75s;
-        //click event on a start btn
-            //start the timer
-                //if timer === 0 game over
-            //start displaying questions one by one
-                //create var index = 0
-                //if (index < qList.length)
-                    //qList[index].question
-                    // for loop over qList[index].options > create html element for qList[index].options[i]
-                //index++
-        //click on options element
-            //Find which element user clicked (eg: event.target, matches("element"))
-            //capture the value on the element (eg : "data-something", "<button value=''>", event.target)
-            //compare the value to the correct answer. > //qList[index].correctAns
-            //if (it's correct){
-                //whatever time left that is user's  score 
-                //display message to the user
-            // else time left - penalty 
-                    //display message to the user
-            //save scores with initials in local storage
-            //start displaying questions one by one
-              //create index var = 0
-            //   if (index < qList.length)
-                    //qList[index].question
-                    // for loop over qList[index].options > create html element for qList[index].options[i]
-    //score.html
-      //capture the score form the local storage and display it to user
-      //reset the local storage with 0.
-var textBox = document.querySelector("#question");
+var questionBox = document.querySelector("#question");
+var answerBox = document.querySelector("#answers");
 var timer = document.querySelector("#timer");
-var btnClick = document.querySelector(".btn");
+var start = document.querySelector("#btnStart");
 var secRemaining = 90;
+var qIndex = 0;
 
-document.querySelector("#btnStart").addEventListener("click", function(e){
-    e.preventDefault();
-
-    timerFunc;
+start.addEventListener("click", function(){
+    timeFunc();
+    questionCreator();
+    start.setAttribute("style", "display: none");
 });
 
-var timerFunc = setInterval(function(){
-    secRemaining--;
-    timer.textContent = ("Time: " + secRemaining);
-
-    if (secRemaining === 0) {
-        clearInterval(timerFunc);
-        textBox.textContent = "Game Over :(";
+function questionCreator() {
+    var qAnswers = qList[qIndex].answers;
+    questionBox.textContent = qList[qIndex].question;
+    answerBox.innerHTML = "";
+    for (let j = 0; j < qAnswers.length; j++){
+        var buttons = document.createElement("button");
+        buttons.innerText = qAnswers[j];
+        buttons.addEventListener("click", checkAnswer);
+        answerBox.append(buttons);
+        console.log(qIndex);
     }
-}, 1000);
+}
 
-// function writeQuestions(e) {
-//     e.preventDefault();
+function timeFunc() {
+    setInterval(function(){
+        secRemaining--;
+        timer.textContent = ("Time: " + secRemaining);
+        if (secRemaining <= 0) {
+            clearInterval(timeFunc);
+            questionBox.textContent = "Game Over :(";
+        }
+    }, 1000);
+}
 
-//     for (let i = 1; i < qList.length; i++) {
-//         textBox.textContent = qList.question1;
-//     }
-// }
-
-// btnClick.addEventListener("click", writeQuestions);
+function checkAnswer(e){
+    if (e.target.innerText === qList[qIndex].correctAnswer) {
+        qIndex++;
+        questionCreator();
+    }
+    else {
+        secRemaining = secRemaining - 10;
+        qIndex++;
+        questionCreator();
+    }
+}
